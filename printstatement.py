e962f6cbc259ad3ec03206d45a2ab011226cc95b
hali67 <warriorsongsa98@gmail.com>
2026-01-11 18:13:15 +1100

    Add files via upload

---
+++ printstatement.py
@@ -0,0 +1,11 @@
+print("Hello, World!!")
+#This is a print statement 
+
+
+
+#Lets print our name
+print("My name is Meredith.")
+
+
+#Lets find the sum of 2 and 5
+print("The sum of 2 and 5 is" , 2 + 5)
