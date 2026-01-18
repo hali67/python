7c8bdaac9d67e458212bb8b9aeb100d6685e2e87
hali67 <warriorsongsa98@gmail.com>
2026-01-18 18:11:23 +1100

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,21 @@
+age = 67
+name = "Papa John"
+
+
+print(name)
+print(age)
+
+
+
+
+codingal = "codingal"
+print(codingal)
+
+
+
+# input statement
+
+# User Input
+name = input("enter your name")
+
+print("Hello" , name , "welcome to codingal!")
