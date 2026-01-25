dbafa61a6ce1c5241e9aa3e39c754aacd4667f12
hali67 <warriorsongsa98@gmail.com>
2026-01-25 17:53:17 +1100

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,11 @@
+myname = "samuel"
+print("type of myname: ",  type(myname)) #string
+
+age = 21
+print("type of age", type(age))
+
+weight = "34.6"
+print("type of weight", type(weight))
+
+isfemale = False
+print("type of d: ", type(isfemale))
