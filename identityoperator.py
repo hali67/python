7f8c739a6ddbc2ddbcbdb61c691e482f6a5c99ec
hali67 <warriorsongsa98@gmail.com>
2026-03-08 11:41:08 +1100

    Add files via upload

---
+++ identityoperator.py
@@ -0,0 +1,33 @@
+x = 5
+
+if (type(x)is int):
+    print("True")
+
+
+else:
+    print("False")
+
+
+    x = 5.5
+
+if (type(x)is not float):
+    
+    print("true")
+
+else:
+    print("False")
+
+
+x = 20
+
+y = 40
+
+if(x is not y):
+
+    print("x and y are not the SAME identity")
+
+
+
+
+
+
