f809bf4c1c59ec248696b87c36a2a12c0a2e22e0
hali67 <warriorsongsa98@gmail.com>
2026-05-10 09:56:14 +1000

    Add files via upload

---
+++ Square.py
@@ -0,0 +1,14 @@
+import turtle
+turtle.Screen().bgcolor("blue")
+turtle.Screen().setup(400,400)
+polygon = turtle.Turtle()
+
+num_sides = 4
+side_length = 200
+angle = 360.0 / num_sides
+
+for i in range(num_sides):
+    polygon.forward(side_length)
+    polygon.right(angle)
+
+turtle.done()
